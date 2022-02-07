03d2286f3382303857dec0fc61cc8fd8d058c665
Tikki <oliver.m.m@hotmail.com>
2022-02-07 16:31:44 +0100

    feat(rules/universal/no-hc-credentials): added function to retrieve left + rhs of declarations

--- src/rules/universal/no-hc-credentials/_rule.ts
+++ src/rules/universal/no-hc-credentials/_rule.ts
@@ -1,10 +1,18 @@
 import { TSESLint, TSESTree, AST_NODE_TYPES } from "@typescript-eslint/utils";
 
+import {
+  isArrayExpression,
+  isArrayPattern,
+  isIdentifier,
+  isLiteral,
+} from "../../../utils/guards";
+
 /**
  * Progress
- *  [x] Detection
+ *  [-] Detection
  *  [ ] Automatic fix / Suggestions
  *  [ ] Reduction of false positives
+ *  [ ] Extensive documentation
  */
 
 /**
@@ -34,43 +42,105 @@ export const noHcCredentials: TSESLint.RuleModule<MessageIds> = {
   },
 
   create: (context) => {
-    console.log(context);
-    /**
-     * Reports to ESLint
-     * @param {TSESTree.Node} node The node to report
-     * @param {string} name The name of the variable
-     */
-    function report(node: TSESTree.Node, name: string) {
+    function report(node: TSESTree.Node) {
       context.report({
-        node: node,
+        node,
         messageId: MessageIds.ERRROR1,
         data: { node },
       });
     }
-    /**
-     * Checks whether a variable name is bad
-     * @param {string} testString The name to check
-     * @returns {Boolean} Whether the name should be reported.
-     */
-    function isPassword(testString: string) {
-      const reg = /^pass(wd|word|code|phrase)?/;
-      return reg.test(testString);
-    }
+
     return {
       VariableDeclarator: (node) => {
-        if (
-          !node.init ||
-          node.id.type !== AST_NODE_TYPES.Identifier ||
-          !node.id.name
-        ) {
+        // TODO: Was is a BindingName?
+        if (!node.init) {
           return;
         }
 
-        if (isPassword(node.id.name)) {
-          //TODO: Reduce FP by removing common test PW
-          report(node, node.id.name);
+        if (isIdentifier(node.id)) {
+          if (isPasswordName(node.id.name)) {
+            report(node.id);
+          }
+        }
+
+        if (isArrayPattern(node.id) && isArrayExpression(node.init)) {
+          const table = retrieveNameAndValues(node.id, node.init);
+          for (const pair of table) {
+            console.log(pair);
+          }
+          table.forEach((pair) => {
+            if (isPasswordName(pair.id?.name)) {
+              report(pair.id);
+            }
+            // TODO: Check value on rhs
+          });
         }
       },
     };
   },
 };
+
+function retrieveNameAndValues(
+  nodeId: TSESTree.ArrayPattern,
+  nodeInit: TSESTree.ArrayExpression
+) {
+  const match: {
+    id: TSESTree.Identifier | null;
+    val: TSESTree.Literal | null;
+  }[] = [];
+
+  for (const element of nodeId.elements) {
+    if (isIdentifier(element)) {
+      match.push({ id: element, val: null });
+    } else {
+      match.push({ id: null, val: null });
+    }
+  }
+
+  for (let i = 0; i < match.length; i++) {
+    const element = nodeInit.elements[i];
+    if (isLiteral(element)) {
+      const innerArray = match[i];
+      if (innerArray != null) {
+        innerArray.val = element;
+      } // No else - val is already null
+    }
+  }
+
+  for (const element of nodeInit.elements) {
+    const innerArray = match[i];
+
+    if (isLiteral(element)) {
+      if (innerArray != null) {
+        innerArray.val = element;
+      } // No else - val is already null
+    }
+  }
+
+  nodeInit.elements.forEach((element, i) => {
+    const innerArray = match[i];
+
+    if (isLiteral(element)) {
+      if (innerArray != null) {
+        innerArray.val = element;
+      }
+    }
+  });
+
+  const res = match.filter(
+    (pair): pair is { id: TSESTree.Identifier; val: TSESTree.Literal } =>
+      pair.id !== null || pair.val !== null
+  );
+
+  return res;
+}
+
+function isPasswordName(testString: string) {
+  const reg = /^pass(wd|word|code|phrase)?/;
+  return reg.test(testString);
+}
+
+function isSafeValue(testString: string) {
+  const reg = /^test/;
+  return reg.test(testString);
+}
