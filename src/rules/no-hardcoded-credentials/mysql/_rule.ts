4177ac4531ce9f0d88c461e34a5f6732d05539a3
Tikki <oliver.m.m@hotmail.com>
2022-02-11 15:14:21 +0100

    feat(rules/no-hardcoded-credentails/mysql): added createPool rule

--- src/rules/no-hardcoded-credentials/mysql/_rule.ts
+++ src/rules/no-hardcoded-credentials/mysql/_rule.ts
@@ -1,5 +1,4 @@
 import { TSESLint, TSESTree } from "@typescript-eslint/utils";
-import { SourceFile } from "typescript";
 
 import {
   isCallExpression,
@@ -40,52 +39,84 @@ export const noHardcodedCredentials: TSESLint.RuleModule<MessageIds> = {
   },
 
   create: (context) => {
+    const test = context;
     const typeProgram = getTypeProgram(context);
-
-    function report(node: TSESTree.Node) {
-      context.report({
-        node,
-        messageId: MessageIds.ERRROR1,
-        data: { node },
-      });
-    }
 
     return {
       VariableDeclarator: (node) => {
-        if (!node.init || !isCallExpression(node.init)) {
+        if (
+          !node.init ||
+          !isCallExpression(node.init) ||
+          !isIdentifier(node.init.callee)
+        ) {
           return;
         }
+        // rhs = createConnection({..})
         const rhs = node.init;
-        if (isIdentifier(rhs.callee)) {
-          const { typeName, returnTypeNames, sourceFile } = getNodeType(
-            typeProgram,
-            rhs.callee
-          );
-          if (
-            typeName == "createConnection" &&
-            returnTypeNames[0] === "Connection" &&
-            sourceFile?.fileName.endsWith("@types/mysql/index.d.ts")
-          ) {
-            const arg = rhs.arguments[0];
-            if (isObjectExpression(arg)) {
-              arg.properties.flatMap((property) => {
-                if (
-                  isProperty(property) &&
-                  isIdentifier(property.key) &&
-                  property.key.name.toLowerCase() === "password"
-                ) {
-                  if (
-                    isLiteral(property.value) &&
-                    !isSafeValue(property.value)
-                  ) {
-                    report(property.value);
-                  }
-                }
-              });
-            }
+        const { typeName, returnTypeNames, sourceFile } = getNodeType(
+          typeProgram,
+          rhs.callee // createConnection
+        );
+
+        // We want to check that we are using the mysql package.
+        if (!sourceFile?.fileName.endsWith("@types/mysql/index.d.ts")) {
+          return;
+        }
+
+        if (
+          typeName === "createConnection" &&
+          returnTypeNames[0] === "Connection"
+        ) {
+          // Assuming that createConnection only contains one arg based on it's
+          // definition.
+          const arg = rhs.arguments[0];
+
+          // If we're using ConnectionConfig
+          if (isObjectExpression(arg)) {
+            checkArgumentsForPassword(arg.properties, context);
           }
+          // TODO: Unhandled connectionURI
+        } else if (typeName === "createPool" && returnTypeNames[0] === "Pool") {
+          // Assuming that createPool only contains one arg
+          // based on it's definition.
+          const arg = rhs.arguments[0];
+
+          // If we're using PoolConfig
+          if (isObjectExpression(arg)) {
+            checkArgumentsForPassword(arg.properties, context);
+          }
+
+          // TODO: Unhandled connectionURI
         }
       },
     };
   },
 };
+
+function checkArgumentsForPassword(
+  properties: TSESTree.ObjectLiteralElement[],
+  context: Readonly<TSESLint.RuleContext<MessageIds, []>>
+) {
+  for (const property of properties) {
+    if (
+      isProperty(property) &&
+      isIdentifier(property.key) &&
+      property.key.name.toLowerCase() === "password"
+    ) {
+      if (isLiteral(property.value) && !isSafeValue(property.value)) {
+        report(property.value, context);
+      }
+    }
+  }
+}
+
+function report(
+  node: TSESTree.Node,
+  context: Readonly<TSESLint.RuleContext<MessageIds, []>>
+) {
+  context.report({
+    node,
+    messageId: MessageIds.ERRROR1,
+    data: { node },
+  });
+}
