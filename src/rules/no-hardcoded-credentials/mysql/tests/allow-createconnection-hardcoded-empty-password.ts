975841d60223b9fb0939eca5f585088911274b33
Tikki <oliver.m.m@hotmail.com>
2022-02-11 15:14:49 +0100

    feat(rules/no-hardcoded-credentails/mysql): added testcases

---
+++ src/rules/no-hardcoded-credentials/mysql/tests/allow-createconnection-hardcoded-empty-password.ts
@@ -0,0 +1,9 @@
+import { createConnection } from "mysql";
+
+const connection = createConnection({
+  host: "localhost",
+  user: "admin",
+  database: "project",
+  password: "",
+  multipleStatements: true,
+});
