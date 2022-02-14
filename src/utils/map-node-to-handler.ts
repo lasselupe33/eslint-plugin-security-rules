d0deb889aa1bfcedc6b175bd22e16266629e1408
Lasse Felskov Agersten <lfa@appear.dk>
2022-02-14 14:44:34 +0100

    feat(utils/map-node-to-handler): allow fallback values, opt-out of warnings

--- src/utils/map-node-to-handler.ts
+++ src/utils/map-node-to-handler.ts
@@ -1,32 +1,45 @@
-import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/utils";
+import { Node, AST_NODE_TYPES } from "@typescript-eslint/types/dist/ast-spec";
 
-export function mapNodeToHandler<
-  Context extends Record<string, unknown> | undefined,
-  ReturnType
->(
-  node: TSESTree.Node | null | undefined,
-  callbacks: {
-    [Type in AST_NODE_TYPES]?: (
-      ctx: Context,
-      node: TSESTree.Node & { type: Type }
-    ) => ReturnType;
-  },
-  ctx?: Context
-): ReturnType | undefined {
-  if (!node) {
-    return;
-  }
+export function makeMapNodeToHandler(
+  { disableWarnings }: { disableWarnings: boolean } = { disableWarnings: false }
+) {
+  return function mapNodeToHandler<
+    Context extends Record<string, unknown>,
+    ReturnType
+  >(
+    node: Node | null | undefined,
+    callbacks: {
+      fallback?: (ctx: Context, node: Node) => ReturnType;
+    } & {
+      [Type in AST_NODE_TYPES]?: (
+        ctx: Context,
+        node: Node & { type: Type }
+      ) => ReturnType;
+    },
+    ctx: Context | undefined = undefined
+  ): ReturnType | undefined {
+    if (!node) {
+      return;
+    }
 
-  const callback = callbacks[node.type];
+    const callback = callbacks[node.type];
 
-  if (!callback) {
-    console.warn(
-      `mapNodeToHandler(${node.type}): No handler associated to type.`,
-      node
-    );
-  }
+    if (!callback && callbacks["fallback"]) {
+      // @ts-expect-error It is okay for the context to be undefined
+      return callbacks["fallback"](ctx, node);
+    }
 
-  // @ts-expect-error Typings are not ideal for the internal implementation of
-  // this helper, however when used externally we get what we expect.
-  return callback?.(ctx, node) as ReturnType;
+    if (!callback && !disableWarnings) {
+      console.warn(
+        `mapNodeToHandler(${node.type}): No handler associated to type.`,
+        node
+      );
+    }
+
+    // @ts-expect-error Typings are not ideal for the internal implementation of
+    // this helper, however when used externally we get what we expect.
+    return callback?.(ctx, node) as ReturnType;
+  };
 }
+
+export const mapNodeToHandler = makeMapNodeToHandler();
