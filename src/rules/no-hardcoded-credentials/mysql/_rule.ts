c0b74220eb23044245b1e08acaf951eda9acb061
Tikki <oliver.m.m@hotmail.com>
2022-02-14 14:34:28 +0100

    feat(rules/no-hardcoded-credentials/mysql): refactored rule to use typesystem later

--- src/rules/no-hardcoded-credentials/mysql/_rule.ts
+++ src/rules/no-hardcoded-credentials/mysql/_rule.ts
@@ -1,16 +1,11 @@
 import { TSESLint, TSESTree } from "@typescript-eslint/utils";
 
-import {
-  isCallExpression,
-  isIdentifier,
-  isObjectExpression,
-  isProperty,
-  isLiteral,
-  isMemberExpression,
-} from "../../../utils/guards";
-import { getNodeType } from "../../../utils/types/get-node-type";
-import { getTypeProgram } from "../../../utils/types/get-type-program";
+import { isIdentifier, isProperty, isLiteral } from "../../../utils/guards";
 import { isSafeValue } from "../utils/is-safe-value";
+
+import { handleIdentifier } from "./handlers/handle-identifier";
+import { extractIdentifier } from "./utils/extract-identifier";
+import { extractObjectProperties } from "./utils/extract-object-properties";
 
 // TODO : Check on AST properties instead of only type properties
 
@@ -23,6 +18,10 @@ import { isSafeValue } from "../utils/is-safe-value";
  *  [ ] Extensive documentation
  *  [ ] Fulfilling configuration options
  */
+
+export type HandlingContext = {
+  ruleContext: Readonly<TSESLint.RuleContext<MessageIds, []>>;
+};
 
 enum MessageIds {
   ERRROR1 = "string",
@@ -42,78 +41,16 @@ export const noHardcodedCredentials: TSESLint.RuleModule<MessageIds> = {
   },
 
   create: (context) => {
-    const typeProgram = getTypeProgram(context);
-    let skipTypescript = true;
-    if (typeProgram) {
-      skipTypescript = false;
-    }
-
-    function handleIdentifier(
-      identifier: TSESTree.Identifier,
-      callExpression: TSESTree.CallExpression,
-      typeName: string
-    ): void {
-      if (identifier.name === "createConnection") {
-        if (!skipTypescript) {
-          if (!(typeName === "Connection")) {
-            return;
-          }
-        }
-        // handle createConnection;
-        handleCallExpression(callExpression);
-      } else if (identifier.name === "createPool") {
-        if (!skipTypescript) {
-          if (!(typeName === "Pool")) {
-            return;
-          }
-        }
-        // handle createPool
-        handleCallExpression(callExpression);
-      }
-    }
-
-    function handleCallExpression(node: TSESTree.CallExpression): void {
-      // Assuming that createConnection only contains one arg based on
-      // it's definition.
-      const arg = node.arguments[0];
-
-      // If we're using ConnectionConfig
-      if (isObjectExpression(arg)) {
-        checkArgumentsForPassword(arg.properties, context);
-      }
-      // TODO: Unhandled connectionURI
-    }
-
     return {
-      VariableDeclarator: (node) => {
-        if (!node.init) {
-          return;
-        }
-        const { typeName, fullyQualifiedName } = getNodeType(
-          typeProgram,
-          node.init
-        );
+      CallExpression: (node) => {
+        const id = extractIdentifier(node);
 
-        if (!skipTypescript) {
-          if (!fullyQualifiedName?.includes("@types/mysql/index")) {
-            return;
-          }
-        }
+        const handled = handleIdentifier({ ruleContext: context }, id);
 
-        // mysql.createConnection or mysql.createPool
-        if (
-          isCallExpression(node.init) &&
-          isMemberExpression(node.init.callee) &&
-          isIdentifier(node.init.callee.property)
-        ) {
-          handleIdentifier(node.init.callee.property, node.init, typeName);
-        }
-        // createConnection or createPool
-        else if (
-          isCallExpression(node.init) &&
-          isIdentifier(node.init.callee)
-        ) {
-          handleIdentifier(node.init.callee, node.init, typeName);
+        if (handled) {
+          const properties = extractObjectProperties(node);
+          checkArgumentsForPassword({ ruleContext: context }, properties);
+          // TODO: Unhandled connectionURI
         }
       },
     };
@@ -121,9 +58,12 @@ export const noHardcodedCredentials: TSESLint.RuleModule<MessageIds> = {
 };
 
 function checkArgumentsForPassword(
-  properties: TSESTree.ObjectLiteralElement[],
-  context: Readonly<TSESLint.RuleContext<MessageIds, []>>
+  ctx: HandlingContext,
+  properties: TSESTree.ObjectLiteralElement[] | undefined
 ) {
+  if (!properties) {
+    return;
+  }
   for (const property of properties) {
     if (
       isProperty(property) &&
@@ -131,17 +71,14 @@ function checkArgumentsForPassword(
       property.key.name.toLowerCase() === "password"
     ) {
       if (isLiteral(property.value) && !isSafeValue(property.value)) {
-        report(property.value, context);
+        report(property.value, ctx);
       }
     }
   }
 }
 
-function report(
-  node: TSESTree.Node,
-  context: Readonly<TSESLint.RuleContext<MessageIds, []>>
-) {
-  context.report({
+function report(node: TSESTree.Node, ctx: HandlingContext) {
+  ctx.ruleContext.report({
     node,
     messageId: MessageIds.ERRROR1,
     data: { node },
