4889f4c122ed3398cbf75c3f5da43bedc59723ac
Tikki <oliver.m.m@hotmail.com>
2022-02-18 13:29:23 +0100

    feat(rules/no-sql-injections/mysql): Tracing
    Started implementation of variable tracing.

--- src/rules/no-sql-injections/mysql/utils/is-escaped-identifier.ts
+++ src/rules/no-sql-injections/mysql/utils/is-escaped-identifier.ts
@@ -1,6 +1,10 @@
 import { AST_NODE_TYPES, TSESTree } from "@typescript-eslint/utils";
+import { getInnermostScope } from "@typescript-eslint/utils/dist/ast-utils";
 
 import { mapNodeToHandler } from "../../../../utils/map-node-to-handler";
+import { traceVariable } from "../../../../utils/tracing/_trace-variable";
+import { makeTraceCallbacksWithTrace } from "../../../../utils/tracing/callbacks/with-current-trace";
+import { printTrace } from "../../../../utils/tracing/utils/printTrace";
 import { getNodeModule } from "../../../../utils/types/get-node-module";
 import { getTypeProgram } from "../../../../utils/types/get-type-program";
 import { HandlingContext } from "../_rule";
@@ -13,13 +17,12 @@ export function isEscapedExpression(
     node,
     {
       [AST_NODE_TYPES.Identifier]: (ctx, identifier) =>
-        // We need variable trace to make sure that the identifier is not
-        // escaped previously.
         isEscapeFunction(ctx, identifier),
       [AST_NODE_TYPES.MemberExpression]: (ctx, memExp) =>
         isEscapedExpression(ctx, memExp.property),
       [AST_NODE_TYPES.CallExpression]: (ctx, callExp) =>
         isEscapedExpression(ctx, callExp.callee),
+      [AST_NODE_TYPES.TemplateElement]: (ctx, callExp) => undefined,
     },
     context
   );
@@ -41,5 +44,42 @@ function isEscapeFunction(
     }
     return true;
   }
-  return false;
+  return isSourceEscaped(context, node);
+}
+
+export function isSourceEscaped(
+  context: HandlingContext,
+  node: TSESTree.Node | undefined
+): boolean {
+  if (!node) {
+    return true;
+  }
+
+  const isSafe = false;
+  const isCurrentTraceSafelySanitzed = false;
+  /**
+   * Iterates through traces to determine whether or not the function has been
+   * escaped.
+   *
+   * We check this by determining if the escape method has been called BEFORE
+   * any modifications in the trace. (Since escaping may be rendered useless
+   * after any modifications)
+   */
+  traceVariable(
+    {
+      context: context.ruleContext,
+      rootScope: getInnermostScope(context.ruleContext.getScope(), node),
+      node,
+    },
+    makeTraceCallbacksWithTrace({
+      onNodeVisited: (trace, traceNode) => {
+        // No op
+      },
+      onTraceFinished: (trace) => {
+        printTrace(trace);
+      },
+    })
+  );
+
+  return isSafe;
 }
