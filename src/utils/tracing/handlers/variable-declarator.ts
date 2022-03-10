9da0f22af84a8b980e1acb0b24ba645210f0f2ba
Lasse Felskov Agersten <lfa@appear.dk>
2022-03-10 11:39:59 +0100

    feat(utils/tracing): add support for array and object destructuring

--- src/utils/tracing/handlers/variable-declarator.ts
+++ src/utils/tracing/handlers/variable-declarator.ts
@@ -1,5 +1,11 @@
 import { TSESTree } from "@typescript-eslint/utils";
 
+import {
+  isArrayPattern,
+  isIdentifier,
+  isObjectPattern,
+  isProperty,
+} from "../../ast/guards";
 import { deepMerge } from "../../deep-merge";
 import { HandlingContext } from "../types/context";
 import { TraceNode } from "../types/nodes";
@@ -15,6 +21,37 @@ export function handleVariableDeclarator(
       astNodes: [...ctx.connection.astNodes, variableDeclarator],
     },
   });
+
+  // In case have encountered a form of destructuring, then ensure we preserve
+  // the destructured memberPath.
+  if (
+    isIdentifier(variableDeclarator.init) &&
+    ctx.connection.variable &&
+    ctx.connection.variable.name !== variableDeclarator.init.name
+  ) {
+    // In the case of array destructing we need to find the relevant index...
+    if (isArrayPattern(variableDeclarator.id)) {
+      const index = variableDeclarator.id.elements.findIndex(
+        (it) => isIdentifier(it) && it.name === ctx.connection.variable?.name
+      );
+
+      if (index !== -1) {
+        nextCtx.meta.memberPath.push(`${index}`);
+      }
+    } else if (isObjectPattern(variableDeclarator.id)) {
+      // ... and in the case of object destructing we need to map to the correct
+      // name.
+      const relevantProperty = variableDeclarator.id.properties.find(
+        (it) =>
+          isIdentifier(it.value) &&
+          it.value.name === ctx.connection.variable?.name
+      );
+
+      if (isProperty(relevantProperty) && isIdentifier(relevantProperty.key)) {
+        nextCtx.meta.memberPath.push(relevantProperty.key.name);
+      }
+    }
+  }
 
   return handleNode(nextCtx, variableDeclarator.init);
 }
