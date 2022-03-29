959224409a7306a529ba01aa039908917fadfc45
Tikki <oliver.m.m@hotmail.com>
2022-03-29 10:31:07 +0200

    fix(rules/no-insecure-ciphers): Redid comment

--- src/rules/no-insecure-ciphers/node/tests/allow-secure-alg.ts
+++ src/rules/no-insecure-ciphers/node/tests/allow-secure-alg.ts
@@ -3,4 +3,4 @@ import { createCipheriv, randomBytes } from "crypto";
 const nonce = randomBytes(12);
 const key = Buffer.from(randomBytes(32));
 
-createCipheriv("AES-256-GCM", key, nonce); // Unsafe
+createCipheriv("AES-256-GCM", key, nonce); // Safe
