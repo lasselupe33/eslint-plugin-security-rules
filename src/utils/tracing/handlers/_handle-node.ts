66155d0d96be1e46f75970bbe7d0974f94ffe5d1
Lasse Felskov Agersten <lfa@appear.dk>
2022-04-22 11:14:01 +0200

    chore(utils/tracing): remove debug output

--- src/utils/tracing/handlers/_handle-node.ts
+++ src/utils/tracing/handlers/_handle-node.ts
@@ -42,7 +42,7 @@ import { handleUnaryExpression } from "./unary-expression";
 import { handleUpdateExpression } from "./update-expression";
 import { handleVariableDeclarator } from "./variable-declarator";
 
-const mapNodeToHandler = makeMapNodeToHandler({ withLogs: true });
+const mapNodeToHandler = makeMapNodeToHandler({ withLogs: false });
 
 export function handleNode(
   context: HandlingContext,
